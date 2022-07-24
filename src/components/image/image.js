cd3f8eec7aa262e496ec1922110c96ae9876210e
Vladisalv Riabichenko <vladislavryabichenko29@gmail.com>
2022-07-24 18:48:04 +0300

    feat: add overlay and hover effect on image

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -1,12 +1,20 @@
 import React from "react";
 import styles from "./styles/image.module.scss";
+import { Link } from "react-router-dom";
 
 export function Image({ data }) {
   // console.log("data", data);
   return (
     <div className={styles.imageWrapper}>
       <img src={data.url} alt="" />
-      <div className={styles.overlay}></div>
+      <div className={styles.overlay}>
+        <Link className={styles.link} to="/image">
+          <button>GO to image</button>
+          {/*<div className={styles.btnWrapper}>*/}
+          {/*  <span className={styles.btn}>GO to Photo</span>*/}
+          {/*</div>*/}
+        </Link>
+      </div>
     </div>
   );
 }
