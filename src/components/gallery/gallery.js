45d5728196623de5a25c7278c303a30d54d22e11
Vladisalv Riabichenko <vladislavryabichenko29@gmail.com>
2022-07-25 18:51:12 +0300

    fix: fix dynamic pafination error (to many requests)

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -9,13 +9,6 @@ import { Loading } from "../loading/loading";
 export function Gallery() {
   const dispatch = useDispatch();
 
-  const images = useSelector((state) => state.gallery.images);
-  const loading = useSelector((state) => state.gallery.status);
-
-  const [page, setPage] = useState(1);
-  const [fetching, setFetching] = useState(true);
-  const [trackScroll, setTrackScroll] = useState(true);
-
   useEffect(() => {
     document.addEventListener("scroll", scrollHandler);
     return function () {
@@ -23,25 +16,35 @@ export function Gallery() {
     };
   }, []);
 
+  const images = useSelector((state) => state.gallery.images);
+  const loading = useSelector((state) => state.gallery.status);
+
+  const [page, setPage] = useState(1);
+  const [fetching, setFetching] = useState(true);
+  const [canBeFetched, setCanBeFetched] = useState(true);
+  const [trackScroll, setTrackScroll] = useState(false);
+
   useEffect(() => {
     if (fetching) {
-      dispatch(getImages(10, page));
+      dispatch(getImages(10, page)).then((data) => {
+        // console.log(data);
+        setFetching(false);
+      });
       setPage(page + 1);
-      setFetching(false);
     }
-    setTrackScroll(true);
   }, [fetching]);
 
+  // useEffect(() => {
+  //   console.log("TRACK FETCHING");
+  // }, [fetching]);
+
   const scrollHandler = (e) => {
-    if (trackScroll) {
-      if (
-        e.target.documentElement.scrollHeight -
-          (e.target.documentElement.scrollTop + window.innerHeight) <
-        100
-      ) {
-        setTrackScroll(false);
-        setFetching(true);
-      }
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+      100
+    ) {
+      setFetching(true);
     }
   };
 
